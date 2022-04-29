315c79308ca8e1169d83204473ecfb2e19dc38a0
byeunghyeun kim <bingu4761@naver.com>
2022-04-29 20:29:35 +0900

    4_29

--- containers/RBTree.hpp
+++ containers/RBTree.hpp
@@ -133,168 +133,188 @@ namespace ft
 			target->_color = Red;
 			insert_fixup(target);
 		};
-		void	insert_fixup_left_node(nodeptr target, nodeptr y)
-		{
-			if (y->_color == Red)						// 삼촌색 Red?
-			{
-				target->_parent->_color = Black;		// 부모색 Black으로 변경
-				y->_color = Black;						// 삼촌색 Black으로 변경
-				target->_parent->_parent->_color = Red;	// 조부모색 Red로 변경
-				target = target->_parent->_parent;		// target을 조부모로 변경
-			}
-			else										// 삼촌색 Black?
-			{
-				if (target == target->_parent->_right)	// target이 오른쪽 노드?
-				{
-					target = target->_parent;			// target을 부모로 변경
-					left_rotation(target);				// 좌회전
-				}
-				target->_parent->_color = Black;		// 부모색 Black으로 변경
-				target->_parent->_parent->_color = Red;	// 조부모색 Red로 변경
-				right_rotation(target);					// 우회전
-			}
-		};
-		void	insert_fixup_right_node(nodeptr target, nodeptr y)
-		{
-			if (y->_color == Red)						// 삼촌색 Red?
-			{
-				target->_parent->color = Black;			// 부모색 Black으로 변경
-				y->_color = Black;						// 삼촌색 Black으로 변경
-				target->_parent->_parent->_color = Red;	// 조부모색 Red로 변경
-				target = target->_parent->_parent;		// target을 조부모로 변경
-			}
-			else										// 삼촌색 Black?
-			{
-				if (target == target->_parent->_left)	// target이 왼쪽 노드?
-				{
-					target = target->_parent;			// target을 부모로 변경
-					right_rotation(target);				// 우회전
-				}
-				target->_parent->_color = Black;		// 부모색 Black으로 변경
-				target->_parent->_parent->_color = Red;	// 조부모색 Red로 변경
-				left_rotation(target);					// 좌회전
-			}
-		};
 		void	insert_fixup(nodeptr target)
 		{												// 부모가 존재하면서 부모의 색이 Red?
 			while (target->_parent->_color == Red)
 			{											// 삼촌이 어느 방향?
 				if (target->_parent == target->_parent->_parent->_left)
-					insert_fixup_left_node(target, target->_parent->_parent->_right);
+				{
+					nodeptr	y = target->_parent->_parent->_right;
+					if (y->_color == Red)						// 삼촌색 Red?
+					{
+						target->_parent->_color = Black;		// 부모색 Black으로 변경
+						y->_color = Black;						// 삼촌색 Black으로 변경
+						target->_parent->_parent->_color = Red;	// 조부모색 Red로 변경
+						target = target->_parent->_parent;		// target을 조부모로 변경
+					}
+					else										// 삼촌색 Black?
+					{
+						if (target == target->_parent->_right)	// target이 오른쪽 노드?
+						{
+							target = target->_parent;			// target을 부모로 변경
+							left_rotation(target);				// 좌회전
+						}
+						target->_parent->_color = Black;		// 부모색 Black으로 변경
+						target->_parent->_parent->_color = Red;	// 조부모색 Red로 변경
+						right_rotation(target);					// 우회전
+					}
+				}
 				else
-					insert_fixup_right_node(target, target->_parent->_parent->_left);
+				{
+					nodeptr	y = target->_parent->_parent->_left;
+					if (y->_color == Red)						// 삼촌색 Red?
+					{
+						target->_parent->color = Black;			// 부모색 Black으로 변경
+						y->_color = Black;						// 삼촌색 Black으로 변경
+						target->_parent->_parent->_color = Red;	// 조부모색 Red로 변경
+						target = target->_parent->_parent;		// target을 조부모로 변경
+					}
+					else										// 삼촌색 Black?
+					{
+						if (target == target->_parent->_left)	// target이 왼쪽 노드?
+						{
+							target = target->_parent;			// target을 부모로 변경
+							right_rotation(target);				// 우회전
+						}
+						target->_parent->_color = Black;		// 부모색 Black으로 변경
+						target->_parent->_parent->_color = Red;	// 조부모색 Red로 변경
+						left_rotation(target);					// 좌회전
+					}
+				}
 			}
 			_root->_color = Black;
 		};
 
+		// Find Node
+		nodeptr	find_node(node_type n)
+		{};
+
 		// Delete
-		nodeptr	sibling(nodeptr target)
+		nodeptr	find_replace_node(nodeptr r)
 		{
-			if (target->_left == _nil)
-				return (target->_parent->_right);
-			else
-				return (target->_parent->_left);
+			if (r->_left == _nil)
+				return (r->_right);
+			else if (r->_right == _nil)
+				return (r->_left);
+			r = r->_right;
+			while (r->_left != _nil)
+				r = r->_left;
+			return (r);
 		};
-		void	replace_node(nodeptr target, nodeptr child)
+		nodeptr	replace_node(nodeptr t, nodeptr r)
 		{
-			child->_parent = target->_parent;
-			if (target->_parent->_left == target)
-				target->_parent->_left = child;
+			r->_parent = t->_parent;
+			if (t->_left == r)					// 대체노드가 현재노드의 왼쪽노드
+			{
+				r->_left = _nil;
+				r->_right = t->_right;
+				if (t->_right != _nil)
+					t->_right->_parent = r;
+			}
+			else if (t->_right == r)			// 대체노드가 현재노드의 오른쪽노드
+			{
+				r->_right = _nil;
+				r->_left = t->_left;
+				if (t->_left != _nil)
+					t->_left->_parent = r;
+			}
 			else
-				target->_parent->_right = child;
+			{
+				r->_parent->_left = r->_right;	// 대체노드와 그 부모에 연결된 노드를
+				if (r->_right != _nil)			// 대체노드의 오른쪽 노드로 교환
+					r->_right->_parent = r->_parent;
+				if (t->_parent->_left == t)		// 대체노드에 부모노드 연결
+					t->_parent->_left = r;
+				else
+					t->_parent->_right = r;
+				r->_left = t->_left;			// 대체노드에 원래 자식들 접목
+				if (t->_left != _nil)
+					t->_left->_parent = r;
+				r->_right = t->_right;
+				if (t->_right != _nil)
+					t->_right->_parent = r;
+			}
 		};
 		void	delete(nodeptr target)
 		{
-			replace_node(target, child);		// target과 child의 위치를 변경한다.
-			if (target->_color == Black)		// target색이 Black?
+			color_t	ori_color = target->_color;
+			nodeptr r = find_replace_node(target);
+			nodeptr	fixup_node = r->_right;
+			color_t	r_color = r->_color;
+			replace_node(r);
+			r->_color = ori_color;
+			if (r_color == Black)				// 빨-빨, 검-빨 ok / 검-검, 빨-검 ko
+				delete_fixup(fixup_node);
+			delete target;
+		};
+		void	delete_fixup(nodeptr x)
+		{
+			while (x != _root && x->_color == Black)
 			{
-				if (child->_color == Red)		// child색이 Red?
-					child->_color = Black;		// child색 Black으로 변경
+				if (x == x->_parent->_left)
+				{
+					nodeptr u = x->_parent->_right;
+					if (u->_color == Red)
+					{
+						u->_color = Black;
+						x->_parent->_color = Red;
+						left_rotation(x->_parent);
+						u = x->_parent->_right;
+					}
+					if (u->_left->_color == Black && u->_right->_color == Black)
+					{
+						u->_color = Red;
+						x = x->_parent;
+					}
+					else
+					{
+						if (u->_right->_color == Black)
+						{
+							u->_left->_color = Black;
+							u->_color = Red;
+							right_rotation(u);
+							u = x->_parent->_right;
+						}
+						u->_color = x->_parent->_color;
+						x->_parent->_color = Black;
+						u->_right->_color = Black;
+						left_rotation(x->_parent);
+						x = _root;
+					}
+				}
 				else
-					delete_case1(target);		// delete case로 입장.
-			}
-			delete target;						// target을 free한다.
-		};
-		void	delete_case1(nodeptr target)
-		{
-			if (target->_parent != this->_nil)	// target이 루트노드가 아닌가?
-				delete_case2(target);
-		};
-		void	delete_case2(nodeptr target)
-		{
-			nodeptr	y = sibling(target);
-			if (y->_color == Red)						// 형제노드의 색이 Red
-			{
-				target->_parent->_color = Red;			// 부모노드의 색을 Red로 변경
-				y->_color = Black;						// 형제노드의 색을 Black으로 변경
-				if (target == target->_parent->_left)	// 해당노드가 왼쪽노드?
-					left_rotation(target->_parent);		// 부모노드를 좌회전
-				else									// 해당노드가 오른쪽노드?
-					right_rotation(target->_parent);	// 부모노드를 우회전
-			}
-			delete_case3(target);
-		};
-		void	delete_case3(nodeptr target)
-		{
-			nodeptr	y = sibling(target);				// 부모노드의 색이 Black, 형제노드의 색이 Black, 형제노드의 자식노드들의 색이 Black
-			if (target->_parent->_color == Black && y->_color == Black
-					&& y->_left->_color == Black && y->_right->_color == Black)
-			{
-				y->_color = Red;						// 형제노드의 색을 Red로 변경
-				delete_case1(target);					// 1case로 리턴
-			}
-			else
-				delete_case4(target);
-		};
-		void	delete_case4(nodeptr target)
-		{
-			nodeptr	y = sibling(target);
-			if (target->_parent->_color == Red && y->_color == Black
-				&& y->_left->_color == Black && y->_right->_color == Black)
-			{
-				y->_color = Red;
-				target->_parent->_color = Black;
-			}
-			else
-				delete_case5(target);
-		};
-		void	delete_case5(nodeptr target)
-		{
-			nodeptr	y = sibling(target);
-			if (y->_color == Black)
-			{
-				if (target == target->_parent->_left && y->_right->_color == Black
-					&& y->_left->_color == Red)
 				{
-					y->_color = Red;
-					y->_left->_color = Black;
-					right_rotation(y);
-				}
-				else if (target == target->_parent->_right && y->_left->_color == Black
-						&& y->_right->_color == Red)
-				{
-					y->_color = Red;
-					y->_right->color = Black;
-					left_rotation(y);
+					nodeptr u = x->_parent->_left;
+					if (u->_color == Red)
+					{
+						u->_color = Black;
+						x->_parent->_color = Red;
+						right_rotation(x->_parent);
+						u = x->_parent->_left;
+					}
+					if (u->_right->_color == Black && u->_left->_color == Black)
+					{
+						u->_color = Red;
+						x = x->_parent;
+					}
+					else
+					{
+						if (u->_left->_color == Black)
+						{
+							u->_right->_color = Black;
+							u->_color = Red;
+							left_rotation(u);
+							u = x->_parent->_left;
+						}
+						u->_color = x->_parent->_color;
+						x->_parent->_color = Black;
+						u->_left->_color = Black;
+						right_rotation(x->_parent);
+						x = _root;
+					}
 				}
 			}
-			delete_case6(target);
-		};
-		void	delete_case6(nodeptr target)
-		{
-			nodeptr	y = sibling(target);
-			y->_color = target->_parent->_color;
-			target->_parent->_color = Black;
-			if (target == target->_parent->_left)
-			{
-				y->_right->_color = Black;
-				left_rotation(target->_parent);
-			}
-			else
-			{
-				y->_left->_color = Black;
-				right_rotation(target->_parent);
-			}
+			x->_color = Black;
 		};
 	};
 };
